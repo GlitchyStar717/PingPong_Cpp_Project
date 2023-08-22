af1b28f19305198fa8349b0909a8acee9cb923d1
GlitchyStar717 <105300650+GlitchyStar717@users.noreply.github.com>
2023-08-22 18:33:58 +0545

    Added error handling in the States::initializeStart() for initializing the SDL VIDEO, TTF, and MIX_OPEN_Audio

--- GameHandler.cpp
+++ GameHandler.cpp
@@ -1,4 +1,5 @@
 #include "GameHandler.h"
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mixer.h>
 #include <SDL2/SDL_ttf.h>
@@ -9,9 +10,17 @@
 void States::initializeStart()
 {
     // Initialize SDL components
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
-    TTF_Init();
-    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+    // Initialize SDL components
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0){
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    }
+    if( TTF_Init() == -1 )
+                {
+                    printf( "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError() );
+                }    
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+    printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
+}
 }
 
 void States::runStartScreen(SDL_Renderer *renderer, TTF_Font *font, TTF_Font *fontTitle, int w, int h, int *winning_points)
