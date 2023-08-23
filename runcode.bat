9211b00ae0a808204d1eb4d0a931d32ba0b614ec
GlitchyStar717 <105300650+GlitchyStar717@users.noreply.github.com>
2023-08-23 20:49:39 +0545

    edited the runcode.bat file as well. Project completes for me now.

--- runcode.bat
+++ runcode.bat
@@ -1,2 +1,4 @@
-g++ main.cpp Game.cpp GameHandler.cpp PlayerScore.cpp Paddle.cpp CheckWallCollision.cpp CheckPaddleCollision.cpp Ball.cpp -I src/include -L src/lib -o Pong -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_mixer -static-libgcc -static-libstdc++
+@echo off
+set MINGW_PATH=%~dp0mingw64\bin
+"%MINGW_PATH%\g++.exe" main.cpp Game.cpp GameHandler.cpp PlayerScore.cpp Paddle.cpp CheckWallCollision.cpp CheckPaddleCollision.cpp Ball.cpp -I src\include -L src\lib -o Pong -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_mixer -static-libgcc -static-libstdc++
 .\Pong
